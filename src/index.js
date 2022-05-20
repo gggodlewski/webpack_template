925654a9f56bcc9385db204451f206b72e0fefa4
gggodlewski <godzilogodlewskigrzegorz@gmail.com>
2022-05-20 13:21:55 +0100

    webpack-setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 // JS Files
-import { getPosts } from './js/getPosts'
+import { getPosts } from "./js/getPosts";
 
 //Style Files (CSS|SASS|SCSS)
-import './styles/style.css'
-import './styles/main.scss'
+import "./styles/style.css";
+import "./styles/main.scss";
 
 //Assest Files (png|jpg|jpeg|gif|svg)
-import twitter_icon from './assets/twitter.svg'
+import twitter_icon from "./assets/twitter.svg";
+(() => {
+  console.log(getPosts().map((item) => item.id));
 
-;(() => {
-  console.log(getPosts())
-
-  const twitterIcon = document.createElement('img')
-  twitterIcon.setAttribute('src', twitter_icon)
-  twitterIcon.style = 'width: 30px; height: 30px;'
-  document.body.append(twitterIcon)
-})()
+  const twitterIcon = document.createElement("img");
+  twitterIcon.setAttribute("src", twitter_icon);
+  twitterIcon.style = "width: 30px; height: 30px;";
+  document.body.append(twitterIcon);
+})();
